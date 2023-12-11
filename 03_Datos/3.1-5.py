ba6de529d3f0b95781d6e68b0b8fe25e939c63b9
Ivan Plis <okivanplis@outlook.com>
2023-12-11 09:49:18 -0300

    Commit de clase 3 - Data

---
+++ 03_Datos/3.1-5.py
@@ -0,0 +1,93 @@
+#%%
+"""
+Debuggear los siguientes fragmentos de codigo
+"""
+# Error de Semantica: tiene_a solo comprueba el primer char y retorna
+
+def tiene_a(expresion):
+    n = len(expresion)
+    i = 0
+    while i<n:
+        if expresion[i] == 'a':
+            return True
+        else:
+            return False
+        i += 1
+
+tiene_a('UNSAM 2020')
+tiene_a('abracadabra')
+tiene_a('La novela 1984 de George Orwell')
+
+#%%
+
+# Error de sintaxis: omitio el ':' 
+
+def tiene_a(expresion)
+    n = len(expresion)
+    i = 0
+    while i<n
+        if expresion[i] = 'a'
+            return True
+        i += 1
+    return Falso
+
+tiene_a('UNSAM 2020')
+tiene_a('La novela 1984 de George Orwell')
+
+#%%
+# Error de Tipos: un integer no tiene longitud como un str
+
+def tiene_uno(expresion):
+    n = len(expresion)
+    i = 0
+    tiene = False
+    while (i<n) and not tiene:
+        if expresion[i] == '1':
+            tiene = True
+        i += 1
+    return tiene
+
+
+tiene_uno('UNSAM 2020')
+tiene_uno('La novela 1984 de George Orwell')
+tiene_uno(1984)
+
+#%%
+# Error de alcance: No retorna nada
+def suma(a,b):
+    c = a + b
+
+a = 2
+b = 3
+c = suma(a,b)
+print(f"La suma da {a} + {b} = {c}")
+
+
+
+#%%
+# Error de Memoria: modifica el mismo dict en cada append
+# En python los tipos de datos son por referencia
+
+import csv
+from pprint import pprint
+
+def leer_camion(nombre_archivo):
+    camion=[]
+    registro={}
+    
+    with open(nombre_archivo,"rt") as f:
+        filas = csv.reader(f)
+        encabezado = next(filas)
+        pprint(encabezado)
+        for fila in filas:
+            # El diccionario registro debería crearse aca
+            registro[encabezado[0]] = fila[0]
+            registro[encabezado[1]] = int(fila[1])
+            registro[encabezado[2]] = float(fila[2])
+            camion.append(registro)
+    return camion
+
+camion = leer_camion('../Data/camion.csv')
+pprint(camion)
+
+# %%
---
+++ 03_Datos/3.9.py
@@ -0,0 +1,54 @@
+"""
+Modificar open_camion .py para que entienda que la columna tiene el precio
+por su encabezado y no por su posición dentro del archivo.
+"""
+
+import csv
+import pprint
+import sys
+
+def open_camion (archivo):
+    """
+    Abre un csv y guarda una lista (camion) de dicts (mercaderias) con los
+    headers como keys
+    """
+
+    camion = []
+
+    with open(archivo, 'rt', encoding='utf-8') as file:
+        rows = csv.reader(file)
+        header = next(rows)
+
+        for n_row, row in enumerate(rows):
+            try:
+                registro = dict(zip(header, row))
+                camion.append(registro)
+            except ValueError:
+                print(f'Fila {n_row}: No pude interpretar: {row}')
+
+    return camion
+
+
+def print_camion(camion):
+    for registro in camion:
+        print(registro)
+
+
+def costo_camion(camion):
+    costo_total = 0
+    for registro in camion:
+        n_cajas = int(registro['cajones'])
+        precio = float(registro['precio'])
+        costo_total += n_cajas * precio
+
+    print(f'El costo total del camion es: {costo_total:>10}')
+
+
+if len(sys.argv) == 2:
+    archivo = sys.argv[1]
+else:
+    archivo = '../Data/camion.csv'
+
+camion = open_camion (archivo)
+print_camion(camion)
+costo_camion(camion)
